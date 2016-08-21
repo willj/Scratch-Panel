1197c0e5637d12ac019d536171ac0be298c42726
Will <willj@users.noreply.github.com>
2016-08-21 12:35:56 +0100

    Revert to a redirect for gh pages index

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require("gulp");
 var uglify = require("gulp-uglify");
 var rename = require("gulp-rename");
-var markdown = require("gulp-markdown");
 
 gulp.task("script", function(){
 	return gulp.src("src/*.js")
@@ -11,16 +10,8 @@ gulp.task("script", function(){
 	.pipe(gulp.dest("dist"));
 });
 
-gulp.task("md", function(){
-	return gulp.src("README.md")
-	.pipe(markdown())
-	.pipe(rename("index.html"))
-	.pipe(gulp.dest("./"));
-});
-
-gulp.task("default", ["script", "md"]);
+gulp.task("default", ["script"]);
 
 gulp.task("watch", function(){
 	gulp.watch("src/*.js", ["default"]);
-	gulp.watch("README.md", ["md"]);
 });
