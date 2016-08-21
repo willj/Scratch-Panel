cbd2337bc70603e9cff358ad18b7ed14f39d9f58
Will <willj@users.noreply.github.com>
2016-08-21 12:18:23 +0100

    Generate index.html from readme for GH Pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require("gulp");
 var uglify = require("gulp-uglify");
 var rename = require("gulp-rename");
+var markdown = require("gulp-markdown");
 
-gulp.task("default", function(){
+gulp.task("script", function(){
 	return gulp.src("src/*.js")
 	.pipe(uglify())
 	.pipe(rename({extname: ".min.js"}))
@@ -10,6 +11,16 @@ gulp.task("default", function(){
 	.pipe(gulp.dest("dist"));
 });
 
+gulp.task("md", function(){
+	return gulp.src("README.md")
+	.pipe(markdown())
+	.pipe(rename("index.html"))
+	.pipe(gulp.dest("./"));
+});
+
+gulp.task("default", ["script", "md"]);
+
 gulp.task("watch", function(){
 	gulp.watch("src/*.js", ["default"]);
+	gulp.watch("README.md", ["md"]);
 });
